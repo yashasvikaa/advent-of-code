a108332fbad70e2f6af9e4a66623db6dea545023
Yashasvika Ahuja <yashasvikaahuja@gmail.com>
2025-06-01 10:56:14 +0530

    Add files via upload

---
+++ 2024/day1_part1.py
@@ -0,0 +1,21 @@
+def reader():
+    grid1 = []
+    grid2 = []
+    with open("day1_part1_input.txt", "r") as file:
+        for line in file: 
+            parts = line.strip().split()
+            grid1.append(int(parts[0]))
+            grid2.append(int(parts[1]))
+    return grid1, grid2
+
+def main():
+    grid1, grid2 = reader()
+    grid1.sort()
+    grid2.sort()
+    sum = 0
+    for i in range(0, len(grid1)):
+        sum += abs(grid1[i] - grid2[i])
+    print(sum)
+
+if __name__ == "__main__":
+    main()
