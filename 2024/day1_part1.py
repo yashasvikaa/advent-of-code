5ede55d62e6b42b59f0b891601eb9e058c1791b0
Yashasvika Ahuja <yashasvikaahuja@gmail.com>
2025-06-01 11:14:22 +0530

    Update day1_part1.py

--- 2024/day1_part1.py
+++ 2024/day1_part1.py
@@ -6,12 +6,12 @@ def reader():
             parts = line.strip().split()
             grid1.append(int(parts[0]))
             grid2.append(int(parts[1]))
+    grid1.sort()
+    grid2.sort()
     return grid1, grid2
 
 def main():
     grid1, grid2 = reader()
-    grid1.sort()
-    grid2.sort()
     sum = 0
     for i in range(0, len(grid1)):
         sum += abs(grid1[i] - grid2[i])
